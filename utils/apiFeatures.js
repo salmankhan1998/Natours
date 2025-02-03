1acd42a301404f841f2ae05fd56f5a26dc1c04ac
salmankhan1998 <skhandilshad1998@gamil.com>
2025-02-04 04:53:34 +0500

    New NDB debug tool added and New route added for invalid routes

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -39,7 +39,7 @@ class APIFeatures {
 
   pagination() {
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 5;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
     this.query = this.query.skip(skip).limit(limit);
     return this;
