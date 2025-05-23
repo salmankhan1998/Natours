1e6771bc1f3e544160d6580c5bf46f23826203f1
salmankhan1998 <skhandilshad1998@gamil.com>
2025-05-23 23:04:05 +0500

    Token Handling Using Cookies and Api Limiting Added For Security

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
@@ -8,18 +10,37 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware
+// ----------------------- Middleware ----------------------------
+
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+//Api Limiting using express-rate-limit for denail of service(DoS) and brurte force attack
+const limiter = rateLimit({
+  max: 100, //Maximum number of requests
+  windowMs: 60 * 60 * 1000, //Expire in 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+//Body parser, reading data from body into req.body
 // this Middleware add the data send in api req into req.body
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+//
 app.use((req, res, next) => {
   // Put date for every request into request object
   req.requestTime = new Date().toISOString();
   next();
 });
-app.use(express.static(`${__dirname}/public`));
 
 // Mounting Router To Path
 app.use('/api/v1/tours', tourRouter);
